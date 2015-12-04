c03a1cf83a7a8f7f419630d039a98e709cb03a22
Mitchell Valine <mitchell.valine@mirumagency.com>
2015-12-04 11:33:43 -0800

    Info popup boxes working. Favicon Added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import 'babel-polyfill';
 import {sdZip} from './sdZip';
+import {mapStyle} from './mapStyle';
 
-let map;
+
+const location = new google.maps.LatLng(32.708798, -117.156004);
+const MAP_TYPE = 'Flux';
+const options = {
+  center: {lat: 32.708798, lng: -117.156004},
+  zoom: 12,
+  mapTypeControlOptions: {
+     mapTypeIds: [google.maps.MapTypeId.ROADMAP, MAP_TYPE]
+  },
+  mapTypeId: MAP_TYPE
+};
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 32.708798, lng: -117.156004},
-    zoom: 12,
+  let fluxMap = new google.maps.StyledMapType({}, {
+    name: 'Flux'
   });
+  let map = new google.maps.Map(document.getElementById('map'), options);
+  map.mapTypes.set(MAP_TYPE, fluxMap);
 
   for(let zip of sdZip) {
     let color;
+    let gallons = 748 * zip.HCF;
+
+    let info = `
+    <h3>${gallons} Gallons = <h3>
+    <ul>
+      <li>${17 * gallons} Showers</li>
+      <li>${7 * gallons} Toliet Flushes</li>
+      <li>${gallons} Teeth Brushings</li>
+    </ul>
+    `;
+
     if(zip.HCF < 25) {
       color = low;
     } else if(zip.HCF >= 25 && zip.HCF < 45) {
@@ -30,16 +53,28 @@ function initMap() {
       strokeWeight: zip.AREA * 50,
       fillColor: color,
       fillOpacity: 0.8,
-      map: map
-    });
+      map: map,
+      });
 
     let mapLabel = new MapLabel({
-      text: `${748 * zip.HCF} Gallons`,
+      text: `${gallons} Gallons`,
       position: new google.maps.LatLng(zip.latitude, zip.longitude),
       map: map,
       fontSize: 20,
       align: 'center',
-      zIndex: 1000
+      zIndex: 1000,
+      strokeWeight: 0,
+      fontColor: '#FFFFFF',
+      minZoom: 13
+    });
+
+    let infowindow = new google.maps.InfoWindow({
+      content: info,
+      position: new google.maps.LatLng(zip.latitude, zip.longitude)
+    });
+
+    circle.addListener('click', function() {
+      infowindow.open(map, circle);
     });
   }
 }
